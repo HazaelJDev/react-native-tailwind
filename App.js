08131a9a6c109f6e25f46a591b296a24de951308
Hazael Jiménez <viajero25057@innovaccion.mx>
2021-03-06 23:31:03 -0600

    add: Installation and configuration of google fonts

--- App.js
+++ App.js
@@ -1,28 +1,60 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
-//import tailwind from 'tailwind-rn'
+import { StyleSheet, Text, View, SafeAreaView, TouchableOpacity } from 'react-native';
 import { tailwind } from './lib/tailwind';
+import { 
+  Roboto_400Regular,
+  Roboto_500Medium,
+} from '@expo-google-fonts/roboto'
+import { 
+  Poppins_600SemiBold,
+  useFonts
+} from '@expo-google-fonts/poppins'
+
 
 export default function App() {
-  return (
-    <SafeAreaView style={tailwind('flex-1 items-center justify-center')}>
-      <View style={tailwind('bg-blue-500 px-5 py-3 rounded-full')}>
-        <Text style={tailwind('text-white font-semibold text-lg')}>
-          Hello RN with Tailwind 👋
-        </Text>
-        <StatusBar style="auto" />
-      </View>
-    </SafeAreaView>
-  );
+  let [fontsLoaded] = useFonts({
+    Poppins_600SemiBold, 
+    Roboto_500Medium, 
+    Roboto_400Regular,
+  });
+  console.log(fontsLoaded)
+  if(!fontsLoaded){
+    return null;
+  }else{
+    console.log(fontsLoaded)
+    return (
+      <SafeAreaView style={tailwind('flex-1 items-center justify-center')}>
+        <View style={tailwind('bg-blue-500 px-5 py-3 rounded-full')}>
+          <Text style={tailwind('text-white font-semibold text-lg')}>
+            Hello RN with Tailwind 👋
+          </Text>
+          <StatusBar style="auto" />
+        </View>
+        <TouchableOpacity style={styles.btnChingon} onPress={() => alert('Hola')}>
+          <Text style={styles.title}>
+            Lista
+          </Text>
+        </TouchableOpacity>
+      </SafeAreaView>
+    );
+  }
 }
 
-/* const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const styles = StyleSheet.create({
+  btnChingon: {
+    ...tailwind('bg-yellow-500 px-5 py-3 rounded-full my-5'),
+  },
+  title: {
+    fontFamily:'Poppins_600SemiBold',
+    fontSize: 60,
+  },
+  subtitle: {
+    fontFamily:'Roboto_500Medium',
+    fontSize: 32,
+  },
+  body: {
+    fontFamily: 'Roboto_400Regular',
+    fontSize: 16,
   },
 });
- */
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,56 @@
 module.exports = {
   purge: [],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: 'class', // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        lightShades: {
+          200: '#FFF599',
+          500: '#F9F9F9',
+          700: '#B79900',
+        },
+        secondary: {
+          200: '#F8EAA5',
+          500: '#FFDC00',
+          700: '#896510',
+        },
+        primary: {
+          200: '#96DCFB',
+          500: '#0074D9',
+          700: '#00429C',
+        },
+        text: {
+          200: '#F1F1F1',
+          500: '#DDDDDD',
+          700: '#858585',
+        },
+        darkShades: {
+          200: '#A0A0A0',
+          500: '#111111',
+          700: '#0A0A0A',
+        },
+        success: {
+          200: '#CFF8A6',
+          500: '#51BF22',
+          700: '#258911',
+        },
+        info: {
+          200: '#96DCFB',
+          500: '#00DAF7',
+          700: '#0080B1',
+        },
+        warning: {
+          200: '#FEF198',
+          500: '#FCCE00',
+          700: '#B58D00',
+        },
+        danger: {
+          200: '#FEC2A9',
+          500: '#F9312A',
+          700: '#B3152A',
+        },
+      }
+    },
   },
   variants: {
     extend: {},
